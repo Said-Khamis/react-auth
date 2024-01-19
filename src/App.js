e77d60b7fb5dff26f685184a1a91deaedccc3b79
Said-Khamis <saidhamic@gmail.com>
2024-01-19 16:56:20 +0300

    DropDown

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Home from './components/Home';
+import ManageUser from './components/pages/ManageUser';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/signup'  element={ <SignUp /> }/>
           <Route path='/' element={ <Layout/> }>
             <Route path='/home' element={ <Home /> }/>
+            <Route path='/manageusers' element={ <ManageUser /> }/>
             <Route path='/settings' element={ <SignUp /> }/>
           </Route>
       </Routes>
--- src/components/DashboardContent.js
+++ src/components/DashboardContent.js
@@ -1,51 +1,58 @@
-import React from 'react'
-import { AiTwotoneShopping  } from 'react-icons/ai';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { 
+  AiTwotoneShopping , 
+  AiOutlineShoppingCart,
+  AiOutlineUsergroupAdd   
+} from 'react-icons/ai';
 
 const DashboardContent = () => {
   return (
     <div className='container-fluid mt-3'>
-        <div className='row g-1 box_dash_parent'>
-            <div className='col-sm-3'>
-              <div className='box_dash'>
-                <AiTwotoneShopping size={60}  color='white'/>
-                <div className='order_item'>
-                  <span> Order (s)</span>
-                    <h2>20000</h2>
-                  </div>
-              </div>
+      <div className='row g-1 box_dash_parent'>
+        <div className='col-sm-3'>
+          <div className='box_dash box_dash_orders'>
+            <AiTwotoneShopping size={60} color='white' />
+            <div className='order_item'>
+              <span> Orders</span>
+              <h2>20,000</h2>
             </div>
-            <div className='col-sm-3'>
-              <div className='box_dash'>
-              <AiTwotoneShopping size={60}  color='white'/>
-               <div className='order_item'>
-                 <span> Order (s)</span>
-                  <h2>20000</h2>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-sm-3'>
-              <div className='box_dash'>
-              <AiTwotoneShopping size={60}  color='white'/>
-               <div className='order_item'>
-                 <span> Order (s)</span>
-                  <h2>20000</h2>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-sm-3'>
-              <div className='box_dash'>
-              <AiTwotoneShopping size={60}  color='white'/>
-               <div className='order_item'>
-                 <span> Order (s)</span>
-                  <h2>20000</h2>
-                </div>
-              </div>
-            </div>
+          </div>
         </div>
+        <div className='col-sm-3'>
+          <div className='box_dash box_dash_users'>
+            <AiOutlineShoppingCart size={60} color='white' />
+            <div className='order_item'>
+              <span> Items</span>
+              <h2>20,000</h2>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-sm-3'>
+          <div className='box_dash box_dash_items'>
+            <AiTwotoneShopping size={60} color='white' />
+            <div className='order_item '>
+              <span> Orders</span>
+              <h2>20,000</h2>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-sm-3'>
+          <div className='box_dash box_dash_clients'>
+            <AiOutlineUsergroupAdd size={60} color='white' />
+            <div className='order_item '>
+              <span> Clients</span>
+              <h2>20,000</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default DashboardContent;
